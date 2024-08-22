6bf5580c5364e3ddac390f3b3920fc945f148efe
Jan <jan810693@gmail.com>
2024-08-22 18:53:55 +0530

    use of class

---
+++ Practice5.cpp
@@ -0,0 +1,19 @@
+// Accessing private member of a class
+#include<iostream>
+using namespace std;
+
+class Circle{
+    private:
+    double radius;
+    public:
+    double area(double r)
+    {
+        radius = r;
+        cout<<radius<<endl;
+        cout<< 3,14*radius*radius;
+    }
+};
+int main(){
+    Circle c;
+    c.area(5);
+}
