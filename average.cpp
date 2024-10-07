c39d2db067b7f2b3cc47095828a83d1e1b1be39d
Jan <jan810693@gmail.com>
2024-10-07 22:35:31 +0530

    average of 5 numbers

---
+++ average.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,f;
+    cout<<"Enter the numbers: ";
+    cin>>a>>b>>c>>d>>e;
+    f=(a+b+c+d+e)/5;
+    cout<<"The average of the numbers is: "<<f;
+    return 0;
+}
