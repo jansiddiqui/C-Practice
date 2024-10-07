c8d41445d4534466ae0e52365c1aec7b6061b806
Jan <jan810693@gmail.com>
2024-10-07 22:27:05 +0530

    square

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the number: ";
+    cin>>a;
+    b=a*a;
+    cout<<"Square of the number is: "<<b;
+    return 0;
+}
