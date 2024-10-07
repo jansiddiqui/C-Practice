152094b7d1d448fcd90588bdaec9f920c7b14c0e
Jan <jan810693@gmail.com>
2024-10-07 22:17:15 +0530

    multiplication

---
+++ multi.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b,c;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    c=a*b;
+    cout<<"The product of a and b is: "<<c;
+    return 0;
+}
