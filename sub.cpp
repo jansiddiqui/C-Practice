0ac36cd9a911cf8334549882cb68ec9705c6d950
Jan <jan810693@gmail.com>
2024-10-07 22:11:31 +0530

    substraction

---
+++ sub.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    c = a-b;
+    cout<<"The difference is: "<<c;
+    return 0;
+}
