e705bf583402b9557ec5f639365787d41b7427a6
Jan <jan810693@gmail.com>
2024-10-07 22:01:51 +0530

    cin and cout

---
+++ cin.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int height;
+    cout<<"Enter the height: ";
+    cin>>height;
+    cout<<"Height of the Boy is: "<<height;
+    return 0;
+}
