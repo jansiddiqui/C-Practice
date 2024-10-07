a2d4688901a2e9993102e88c4e868e9d553064ce
Jan <jan810693@gmail.com>
2024-10-07 22:22:06 +0530

    division

---
+++ division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b,c;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    c=a/b;
+    cout<<"The division of a & b is: "<<c;
+    return 0;
+}
