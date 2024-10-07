baf3a8bcb60a31fe3d90e6dc04e55d036201b893
Jan <jan810693@gmail.com>
2024-10-07 22:55:45 +0530

    name

---
+++ name.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string firstname, lastname;
+    cout<<"Enter your first name: ";
+    cin>>firstname;
+    cout<<"Enter your last name: ";
+    cin>>lastname;
+    cout<<"Hello, "<<firstname<<" "<<lastname;
+    return 0;
+}
