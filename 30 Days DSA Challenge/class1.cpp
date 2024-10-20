d22fa2d63da3001e396716f693acaccf2b06c2c7
Jan <jan810693@gmail.com>
2024-10-20 19:17:51 +0530

    Car

--- 30 Days DSA Challenge/class1.cpp
+++ 30 Days DSA Challenge/class1.cpp
@@ -17,12 +17,19 @@ class Car{
 int main(){
 
     Car car;
+    Car car1;
 
     car.brand="TATA";
     car.model="Nexon";
     car.year=2020;
-
+    cout<<"Car 1 :)"<<endl;
     car.displayCarInfo();
 
+    car1.brand="Hyundai";
+    car1.model="Creta";
+    car1.year=2018;
+    cout<<"Car 2 :)"<<endl;
+    car1.displayCarInfo();
+    
     return 0;
 }
