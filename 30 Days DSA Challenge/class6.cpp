a2a3e960fed94ee87aa2527c5e4cbfda22ae4f82
Jan <jan810693@gmail.com>
2024-10-21 00:05:49 +0530

    Grade Calculation

---
+++ 30 Days DSA Challenge/class6.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class StudentGrade{
+    public:
+        string name;
+        string subject;
+        int marks;
+    public:
+        void calculateGrade(){
+            if(marks>=90){
+                cout<<"Grade: A"<<endl;
+            }else if(marks>=75){
+                cout<<"Grade: B"<<endl;
+            }else if(marks>=50){
+                cout<<"Grade: C"<<endl;
+            }else if(marks<50){
+                cout<<"Grade: F"<<endl;
+            }
+        }
+        void displayStudentInfo(){
+            cout<<"Name: "<<name<<endl;
+            cout<<"Subject: "<<subject<<endl;
+            cout<<"Marks: "<<marks<<endl;
+        }
+};
+int main(){
+    StudentGrade s1;
+    s1.name="John";
+    s1.subject="Maths";
+    s1.marks=90;
+    s1.displayStudentInfo();
+    s1.calculateGrade();
+    return 0;
+
+}
