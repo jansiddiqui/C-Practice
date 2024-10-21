4e9fbb6660e8baf5870e39f1f3dad986b7e2356a
Jan <jan810693@gmail.com>
2024-10-21 21:55:28 +0530

    function

---
+++ Function/Structure.cpp
@@ -0,0 +1,5 @@
+//return_type function_name(parameter_list){
+    //function body
+    //Code to execute
+    //return value; //optional if return_type is void
+//}
---
+++ Function/function.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int addition(int a, int b){
+    return a+b;
+}
+int main(){
+    int result=addition(5,3);
+    cout<<"The result is "<<result<<endl;
+    return 0;
+}
